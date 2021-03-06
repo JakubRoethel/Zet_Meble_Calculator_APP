b7b946fb30a55ffbacfc7f56e0c4a4f21920c71b
stanleykubrik1 <62182076+stanleykubrik1@users.noreply.github.com>
2021-03-06 20:39:47 +0100

    PDF PRINT

---
+++ src/components/PrintPDFTest2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+
+const PrintPDFTest2 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PrintPDFTest2
