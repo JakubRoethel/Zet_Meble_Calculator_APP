1a8f48da999a57171688192a90b419ca78c18e9e
stanleykubrik1 <62182076+stanleykubrik1@users.noreply.github.com>
2021-04-21 22:33:04 +0200

    media queries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
 
 
       <nav class="navbar navbar-expand-lg navbar-light header">
-      <button class="navbar-toggler ms-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
    
